9bf8bc3686535cd30035103c3886fbcabe7e9062
Vui-Chee <vuicheesiew@gmail.com>
2020-11-18 21:53:16 +0800

    Impl Sorter trait for merge sort

--- src/sort/mergesort.rs
+++ src/sort/mergesort.rs
@@ -1,72 +1,84 @@
-#[allow(dead_code)]
-pub fn merge<T>(arr: &mut [T], s1: usize, s2: usize, end: usize)
-where
-    T: Ord + Copy,
-{
-    let mut temp = Vec::<T>::new();
+use super::Sorter;
 
-    // Copy first sorted array into temp vector.
-    for index in s1..s2 {
-        temp.push(arr[index]);
-    }
+struct MergeSort;
 
-    let mut i = 0;
-    let mut j = s2;
-    let mut k = s1;
-    while i < temp.len() && j <= end {
-        if temp[i] < arr[j] {
-            arr[k] = temp[i];
-            i += 1;
-        } else {
-            arr[k] = arr[j];
-            j += 1;
+impl MergeSort {
+    fn merge<T>(arr: &mut [T], s1: usize, s2: usize, end: usize)
+    where
+        T: Ord + Copy,
+    {
+        let mut temp = Vec::<T>::new();
+
+        // Copy first sorted array into temp vector.
+        for index in s1..s2 {
+            temp.push(arr[index]);
         }
-        k += 1;
+
+        let mut i = 0;
+        let mut j = s2;
+        let mut k = s1;
+        while i < temp.len() && j <= end {
+            if temp[i] < arr[j] {
+                arr[k] = temp[i];
+                i += 1;
+            } else {
+                arr[k] = arr[j];
+                j += 1;
+            }
+            k += 1;
+        }
+
+        // Copy remaining elements from temp (if any)
+        for index in i..temp.len() {
+            arr[k] = temp[index];
+            k += 1;
+        }
     }
 
-    // Copy remaining elements from temp (if any)
-    for index in i..temp.len() {
-        arr[k] = temp[index];
-        k += 1;
+    pub fn merge_sort_helper<T>(arr: &mut [T], start: usize, end: usize)
+    where
+        T: Ord + Copy,
+    {
+        if start >= end {
+            return;
+        }
+
+        let mid = (start + end) / 2;
+        MergeSort::merge_sort_helper(arr, start, mid);
+        MergeSort::merge_sort_helper(arr, mid + 1, end);
+        MergeSort::merge::<T>(arr, start, mid + 1, end);
     }
 }
 
-/// Merge sort in place
-#[allow(dead_code)]
-pub fn mergesort<T>(arr: &mut [T], start: usize, end: usize)
-where
-    T: Ord + Copy,
-{
-    if start >= end {
-        return;
+impl Sorter for MergeSort {
+    fn sort<T>(arr: &mut [T])
+    where
+        T: Ord + Copy,
+    {
+        if arr.len() > 0 {
+            MergeSort::merge_sort_helper(arr, 0, arr.len() - 1);
+        }
     }
-
-    let mid = (start + end) / 2;
-    mergesort(arr, start, mid);
-    mergesort(arr, mid + 1, end);
-    merge::<T>(arr, start, mid + 1, end);
 }
 
 #[cfg(test)]
 mod tests {
-    use super::mergesort;
+    use super::super::sort;
+    use super::MergeSort;
 
     #[quickcheck]
     fn test_mergesort(xs: Vec<isize>) -> bool {
         let mut arr = xs.to_owned();
         let size = arr.len();
 
-        // Cannot run on empty array - like what for?
-        if size <= 0 {
-            return true;
-        }
+        sort::<_, MergeSort>(&mut arr);
 
-        mergesort(&mut arr, 0, size - 1);
-
-        // Check ordering.
-        for i in 0..size - 1 {
-            if arr[i] > arr[i + 1] {
-                return false;
+        if size > 0 {
+            // Check ordering.
+            for i in 0..size - 1 {
+                if arr[i] > arr[i + 1] {
+                    return false;
+                }
             }
         }
 
--- src/sort/mod.rs
+++ src/sort/mod.rs
@@ -1,1 +1,15 @@
 pub mod mergesort;
+
+pub trait Sorter {
+    fn sort<T>(arr: &mut [T])
+    where
+        T: Ord + Copy;
+}
+
+pub fn sort<T, S>(arr: &mut [T])
+where
+    T: Ord + Copy,
+    S: Sorter,
+{
+    S::sort(arr);
+}
